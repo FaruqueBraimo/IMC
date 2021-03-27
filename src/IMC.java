94d9181ff0b33249a123f0f246a1247c0d755b0b
Faruque Braimo <faruquebraimo@gmail.com>
2021-03-27 18:27:03 +0200

    Initial commit

---
+++ src/IMC.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author faruq
+ */
+import javax.swing.JOptionPane;
+
+public class IMC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Tela t  = new Tela();
+       t.setVisible(true);
+
+
+            
+    }
+
+}
